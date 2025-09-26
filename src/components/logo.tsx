2e4263a79f973637ccdc234a8b44aa79c554e906
Leandro Chena <leandro.airedesantafe@gmail.com>
2025-09-26 21:53:38 +0000

    Image with src "/aire-logo.png" was detected as the Largest Contentful P

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export function Logo() {
   return (
     <div className="flex items-center gap-2.5 font-bold">
-      <Image src="/aire-logo.png" alt="Logo de Aire de Santa Fe" width={32} height={32} className='rounded-md' />
+      <Image src="/aire-logo.png" alt="Logo de Aire de Santa Fe" width={32} height={32} className='rounded-md' priority />
       <span className="font-headline text-xl text-foreground">CRM Aire</span>
     </div>
   );
