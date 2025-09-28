2f35e0c630faf6d6fc754195fc5a612fde989726
Leandro Chena <leandro.airedesantafe@gmail.com>
2025-09-28 19:11:14 +0000

    Sigue así

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -8,7 +8,7 @@ type HeaderProps = {
 
 export function Header({ title, children }: HeaderProps) {
   return (
-    <header className="sticky top-0 z-50 flex h-16 items-center gap-4 border-b bg-background/80 px-4 backdrop-blur-sm md:px-6">
+    <header className="sticky top-0 z-10 flex h-16 items-center gap-4 border-b bg-background/80 px-4 backdrop-blur-sm md:px-6">
       <SidebarTrigger className="md:hidden" />
       <h1 className="flex-1 text-2xl font-bold font-headline tracking-tight">
         {title}
