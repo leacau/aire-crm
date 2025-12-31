0db71a23168cce8380d2a7519d28154fb82b3d5b
leacau <97464095+leacau@users.noreply.github.com>
2025-12-31 12:20:14 -0300

    Update console logs for request start date

--- src/app/licencias/page.tsx
+++ src/app/licencias/page.tsx
@@ -197,7 +197,9 @@ export default function LicensesPage() {
     const today = new Date();
     const todayFormatted = format(today, "d 'de' MMMM 'de' yyyy", { locale: es });
     const start = format(new Date(request.startDate), "d 'de' MMMM 'de' yyyy", { locale: es });
-    console.log('requestday formated ', format(request.startDate, "d 'de' MMMM 'de' yyyy"));
+    console.log('requestday formated: ', request.startDate);
+    console.log('requestday formated + 1: ', (request.startDate + 1));
+    console.log('requestday newDate: ', new Date(request.startDate));
     const end = format(new Date(request.endDate), "d 'de' MMMM 'de' yyyy", { locale: es });
     const returnDate = format(new Date(request.returnDate), "d 'de' MMMM 'de' yyyy", { locale: es });
     const remaining = pendingDays ?? applicant?.vacationDays ?? 0;
