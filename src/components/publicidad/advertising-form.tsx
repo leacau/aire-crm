d0950b817ccfdc612750aea0d3ecbdff45ba6157
leacau <97464095+leacau@users.noreply.github.com>
2026-03-06 07:42:38 -0300

    Update advertising-form.tsx

--- src/components/publicidad/advertising-form.tsx
+++ src/components/publicidad/advertising-form.tsx
@@ -483,7 +483,7 @@ export function AdvertisingForm() {
 
                   await sendEmail({
                       accessToken,
-                      to: ['lchena@airedesantafe.com.ar', 'alucca@airedesantafe.com.ar', 'materiales@airedesantafe.com.ar'], 
+                      to: ['lchena@airedesantafe.com.ar', 'alucca@airedesantafe.com.ar', 'materiales@airedesantafe.com.ar', userInfo.email], 
                       subject: `${editModeId ? 'Modificación' : 'Nueva'} OP: ${oppTitle} - ${selectedClient?.denominacion}`,
                       body: emailBody,
                       attachments: [{
